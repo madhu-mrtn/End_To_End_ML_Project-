fcd834f71879b926660a87d9b4cbd33938fc01a2
Madhu <madhu.chauhan.prf@gmail.com>
2023-08-12 19:56:55 +0530

    Data transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,23 +2,24 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts',"train.csv")
-    ## Data ingestion will store the train data in the folder artifacts
-    test_data_path: str = os.path.join('artifacts',"test.csv")
-    raw_data_path: str = os.path.join('artifacts',"data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
-        # The three paths will get saved in this variable
 
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
@@ -30,22 +31,31 @@ class DataIngestion:
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            logging.info("train test split initiates")
-            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of data is completed")
+            logging.info("Inmgestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+
             )
         except Exception as e:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    
+    
+
 
